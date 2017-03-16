69f4d7b656606d0fa64e11c6ce70bfb92f1930e1
Lilly Ileso <Lilly_Ileso@hotmail.com>
2017-03-16 23:27:09 +0000

    Add test files

---
+++ FizzBuzz/FizzBuzz.Tests/Tests.cs
@@ -0,0 +1,30 @@
+﻿using FizzBuzz.Library;
+using NUnit.Framework;
+
+namespace FizzBuzz.Tests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void WhenNotAMultipleOf5And3ReturnNothing([Values(1, 2, 4, 7, 8, 11, 13, 14, 16, 17, 19)] int input)
+        {
+            string output = FizzBuzzBasic.GetValue(input);
+            Assert.AreEqual(string.Empty, output);
+        }
+
+        [Test]
+        public void WhenNumIsMultipleOf3ReturnFizz([Values(3, 6, 9, 12, 18)] int input)
+        {
+            string output = FizzBuzzBasic.GetValue(input);
+            Assert.AreEqual("Fizz", output);
+        }
+
+        [Test]
+        public void WhenNumIsMultipleOf5ReturnBuzz([Values(5, 10)] int input)
+        {
+            string output = FizzBuzzBasic.GetValue(input);
+            Assert.AreEqual("Buzz", output);
+        }
+    }
+}
--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FizzBuzz.Library;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
