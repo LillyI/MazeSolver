9d50deb39af56292a7895de81d9bd3f6b442c6bb
Lilly Ileso <Lilly_Ileso@hotmail.com>
2017-03-17 00:02:07 +0000

    Refactor GetValue method some more.

--- FizzBuzz/FizzBuzz.Library/FizzBuzzBasic.cs
+++ FizzBuzz/FizzBuzz.Library/FizzBuzzBasic.cs
@@ -10,12 +10,8 @@ namespace FizzBuzz.Library
     {
         public static string GetValue(int input)
         {
-            string output = string.Empty;
-
-            output += (input % 3) == 0 ? "Fizz" : "";
-            output += (input % 5) == 0 ? "Buzz" : "";
-
-            return output;
+            return ((input % 3) == 0 ? "Fizz" : string.Empty) + 
+                ((input % 5) == 0 ? "Buzz" : "");
         }
     }
 }
